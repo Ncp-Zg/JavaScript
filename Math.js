8ccfaca24c4ea4d0976a846d533806a4b11c8755
Necip <ncptzgn@gmail.com>
2021-02-14 17:55:10 +0300

    JavaScript Beginning

---
+++ Math.js
@@ -0,0 +1,50 @@
+let value ;
+
+const value1 = 10;
+
+const value2 = 9;
+
+//Aritmetik Operatörler
+
+// value = value1 +value2;
+// value = value1 - value2;
+// value = value1 * value2;
+// value = value1 / value2;
+// value = value1 % value2;
+
+// Math Objesi
+
+value = Math.PI;
+value = Math.E;
+
+value = Math.round(3.6);
+value = Math.round(3.4);
+value = Math.round(3.2);
+
+
+value = Math.ceil(3.2);
+value = Math.ceil(3.7);
+
+value = Math.floor(3.2);
+value = Math.floor(3.7);
+
+value = Math.sqrt(16);
+value = Math.sqrt(84);
+
+value = Math.abs(-10);
+
+value = Math.pow(8,2);
+value = Math.pow(2,8);
+
+
+value = Math.max(10,-1,100,32);
+value = Math.min(10,-1,100,32);
+
+value = Math.random();
+
+value = Math.floor(Math.random() *20 +1);
+
+
+
+
+console.log(value);
