d66ae41f255a2b3ab1e138d6c911718a448f18d9
Necip <ncptzgn@gmail.com>
2021-02-14 17:20:51 +0300

    JavaScript Beginning

---
+++ var-let-const.js
@@ -0,0 +1,39 @@
+// Var
+
+// var name = "Mehmet";
+
+// console.log(name);
+
+// name = "Mustafa"
+
+// console.log(name);
+
+// //Let
+
+// let a,b ;
+
+// a=10;
+// b=20;
+
+// console.log(a+b);
+
+//Const
+
+// const name ="Mehmet";
+// console.log(name);
+// name = "Murat";
+// console.log(name);
+
+// const a;
+// a = 10;
+// console.log(a);
+
+const a =[1,2,3,4,5];
+console.log(a);
+a.push(6);
+
+const b = 10;
+
+b=20;
+
+console.log(a);
