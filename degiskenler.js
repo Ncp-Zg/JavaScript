de8adc2afe4daf26b6530e2bc780bf0c2a8e895c
Necip <ncptzgn@gmail.com>
2021-02-14 13:54:15 +0300

    JavaScript Beginning

---
+++ degiskenler.js
@@ -0,0 +1,93 @@
+//Degisken Olusturma
+
+// var a = 20;
+// var b = 10;
+// var c = 40;
+
+// console.log(a,b,c)
+
+
+
+//Primitive Veri Tipleri
+
+// var a = 10; //Nember Veri Tipi
+// var b = 3.14 // Number Veri Tipi
+// console.log(typeof a);
+// console.log(typeof b);
+
+//String
+
+// var name = "Mehmet";
+
+// console.log(name);
+// console.log(typeof name);
+
+// Boolean
+
+// var a = true;
+
+// console.log(typeof a)
+
+
+// var a = null;
+
+// console.log(a);
+
+// console.log(typeof a);
+
+// var a;
+// console.log(a);
+
+
+
+//Reference Veri Tipleri
+
+// var numbers = [1,2,3,4,5];
+// console.log(numbers);
+// console.log(typeof numbers);
+// console.log(numbers[0]);
+
+// var person = {
+//     name: "Mehmet",
+//     age : 25
+// }
+
+// console.log(person);
+// console.log(typeof person);
+
+// var date = new Date();
+
+// console.log(date);
+
+// console.log(typeof date);
+
+// var merhaba = function(){
+//     console.log("Merhaba");
+// }
+
+// console.log(merhaba);
+// console.log(typeof merhaba);
+
+
+
+//Primitive veri tipi
+// var a= 10; 
+// var b= a;
+
+// console.log(a,b);
+
+// a= 20;
+
+// console.log(a,b);
+
+
+//Reference veri tipi
+
+var a = [1,2,3];
+
+
+var b = a;
+
+a.push(4);
+console.log(b);
+
