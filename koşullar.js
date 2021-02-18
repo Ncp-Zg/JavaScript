94b1df8e040c2fec6b47ed57cc2bcb996b3a0fab
Necip <ncptzgn@gmail.com>
2021-02-18 15:37:48 +0300

    JavaScript Beginning

---
+++ koşullar.js
@@ -0,0 +1,96 @@
+// Karşılaştırma operatörleri
+
+//==
+//===
+//!=
+//!==
+//>
+//>
+//>=
+//<=
+
+
+// console.log(2 == 2) ;
+// console.log("js" == "java");
+
+// console.log(2 == "2");
+// console.log(2 === "2");
+
+// console.log(4 > 2);
+// console.log(2 > 4);
+
+//console.log(2 != 2);
+
+//console.log(2 < 4);
+// console.log(4 < 2);
+
+//console.log(2 >= 4);
+
+//console.log(4 <= 2)
+
+//Mantıksal Bağlaçlar
+
+//Not Operatörü
+
+// console.log(!(2 !== 2));
+
+//And Operatörü
+
+// console.log( (4 == 2) && ("Ahmet" == "Ahmet"));
+
+// //Or Operatörü
+
+// console.log((4 == 2) || ("Ahmet" !== "Ahmet"));
+
+// const error = false;
+
+// if (error == true) {
+//     console.log("Hata Oluştu")
+// }
+// else {
+//     console.log("Hata Oluşmadı")
+// }
+
+// const user = "mmc";
+
+// if (user == "mmc") {
+//     console.log("Kullanıcı Bulundu");
+// }
+// else{
+//     console.log("Kullanıcı Bulunmadı...");
+// }
+
+// const process = "3";
+
+// if (process === "1"){
+//     console.log("İşlem 1");
+// }
+// else if (process === "2"){
+//     console.log("İşlem 2");
+// }
+// else if (process === "3"){
+//     console.log("İşlem 3");
+// }
+// else if (process === "4"){
+//     console.log("İşlem 4");
+// }
+// else{
+//     console.log("Geçersiz İşlem")
+// }
+
+const number = 100;
+
+// if ( number === 100) {
+//     console.log("Sayı 100'e eşit.")
+// }
+// else{
+//     console.log("Sayı 100'e eşit değil.")
+// }
+
+//Ternary Operator
+
+// console.log(number === 100 ? "Sayı 100" : "Sayı 100 Değil")
+
+
+if(number === 100) console.log("Sayı 100");
+else console.log("Sayı 100 değil")
