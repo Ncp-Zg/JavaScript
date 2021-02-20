0c115dfd80668abec092b8d6307b5d017f39bf52
Necip <ncptzgn@gmail.com>
2021-02-20 13:16:48 +0300

    JavaScript Beginning

---
+++ elementsecme.js
@@ -0,0 +1,38 @@
+// Elementi Id ye göre seçme
+
+let element;
+
+element = document.getElementById("todo-form");
+element = document.getElementById("tasks-title");
+
+// Element Class'a göre seçme
+
+element = document.getElementsByClassName("list-group-item")[0];
+element = document.getElementsByClassName("card-header");
+
+// Element Tag'e Göre Seçme
+
+element = document.getElementsByTagName("div");
+
+// Query Selector - css Selector -Tek Bir Element
+
+element = document.querySelector("#todo-form");
+element = document.querySelector("#tasks-title");
+
+
+element = document.querySelector(".list-group-item");
+
+element = document.querySelector("li")
+element = document.querySelector("div")
+
+//QuerySelectorAll -Tüm Elementleri Seç  
+
+element = document.querySelectorAll(".list-group-item");  //Node List
+
+element.forEach(function(el){
+    console.log(el);
+});
+
+
+
+console.log(element);
