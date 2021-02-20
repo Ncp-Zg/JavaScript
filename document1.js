7205cca7aa2996d8699edff5d937cfe191cc16cd
Necip <ncptzgn@gmail.com>
2021-02-20 11:57:06 +0300

    JavaScript Beginning

---
+++ document1.js
@@ -0,0 +1,37 @@
+// Window Objesi
+let value;
+
+
+value = document;
+value = document.all;
+value = document.all.length;
+value = document.all[0];
+value = document.all[6];
+value = document.all[document.all.length-1];
+
+const elements = document.all; //Html Collection
+// for(let i = 0; i < elements.length;i++){
+// console.log(elements[i]);
+// }
+
+// const collections = Array.from(document.all);
+
+// collections.forEach(function(collection){
+//     console.log(collection);
+// });
+
+value = document.all[6];
+value = document.body;
+value = document.head;
+value = document.location;
+value = document.hostname;
+value = document.location.port;
+
+value = document.URL;
+
+value = document.characterSet;
+
+
+
+
+console.log(value);
