05c90d20f01eef0aec3a8a24cbd28db5de771079
Necip <ncptzgn@gmail.com>
2021-02-20 21:24:31 +0300

    JavaScript Beginning

---
+++ Dinamiközellikekleme.js
@@ -0,0 +1,26 @@
+const todoInput = document.getElementById("todo");
+let element;
+
+
+
+element = todoInput;
+element = todoInput.classLists;
+
+// todoInput.classList.add("newClass");
+// todoInput.classList.add("newClass2");
+// todoInput.classList.remove("form-control");
+
+
+
+
+element = todoInput;
+element = todoInput.placeholder;
+element = todoInput.getAttribute("placeholder");
+todoInput.setAttribute("placeholder", "yazıyor...");
+todoInput.setAttribute("title", "Input");
+todoInput.removeAttribute("name");
+element = todoInput;
+// element = todoInput.hasAttribute("name");
+
+
+console.log(element);
